a6a3465314d534c97b345f907a349843b06dc225
annutiwari <107735048+annutiwari@users.noreply.github.com>
2022-11-10 15:45:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+import Tvmaze from "./component/TVMaze/TVmaze";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <Comment /> */}
+      {/* <Season /> */}
+      {/* <Pics /> */}
+      {/* <Widget /> */}
+      {/* <Songs /> */}
+      <Tvmaze />
+    </div>
+  );
+}
